80d447eea606cbbc4f33a4dc8efcdd49b8c9c556
Kéven <69770399+KevenChalesPereira@users.noreply.github.com>
2024-08-30 21:41:24 -0300

    Add files via upload

---
+++ www/js/index.js
@@ -0,0 +1,20 @@
+document.addEventListener("scrollend", (event) => {
+ document.getElementById("titulo").innerText = "";
+document.getElementById("num").value = "";
+ document.getElementById("corpo").innerText = "";
+});
+
+
+});
+
+function mostrar() {
+    var num = document.getElementById("num").value;
+    
+    fetch(`https://jsonplaceholder.typicode.com/posts/${num}`)
+        .then(response => response.json())
+        .then(data => {
+          document.getElementById("titulo").innerText = data.title;
+        document.getElementById("corpo").innerText = data.body;
+        })
+        .catch(error => console.error('Erro ao buscar dados:', error));
+}
